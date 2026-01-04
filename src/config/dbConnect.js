463ee6136c68376261cb2379577cc9bb8a7aff51
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-01-04 22:16:49 +0600

    added assets and config daisy UI and mongodb connection

---
+++ src/config/dbConnect.js
@@ -0,0 +1,29 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+let cachedClient = null;
+let cachedDb = null;
+
+export const connectDB = async () => {
+  if (cachedClient && cachedDb) return cachedDb;
+
+  const client = new MongoClient(process.env.DATABASE_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  await client.connect();
+  const db = client.db(process.env.DB_NAME);
+
+  console.log("MongoDB Connected");
+
+  cachedClient = client;
+  cachedDb = db;
+  return db;
+};
+
+export const collections = {
+  PRODUCTS: async () => (await connectDB()).collection("products"),
+};
