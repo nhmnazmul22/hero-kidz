9d2ba94dd5a4d6f2ac44b24047cf918c98f21a5f
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-01-09 22:50:08 +0600

    checkout form manage successful

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -4,28 +4,30 @@ import CheckoutHeader from "@/components/checkout/CheckoutHeader";
 import CheckoutItems from "@/components/checkout/CheckoutItems";
 import OrderSummary from "@/components/order/OrderSummary";
 import CartContextProvider from "@/context/CartContext/CartContextProvider";
+import CheckoutContextProvider from "@/context/CheckoutContext/CheckoutContextProvider";
 
 const CheckoutPage = async () => {
   const result = await getCarts();
-
   const cartItems = result.data || [];
 
   return (
     <CartContextProvider initialItems={cartItems}>
-      <div className="main-container py-10">
-        <CheckoutHeader />
+      <CheckoutContextProvider>
+        <div className="main-container py-10">
+          <CheckoutHeader />
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-8">
-          {/* Left */}
-          <div className="lg:col-span-2 space-y-6">
-            <CheckoutForm />
-            <CheckoutItems />
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-8">
+            {/* Left */}
+            <div className="lg:col-span-2 space-y-6">
+              <CheckoutForm />
+              <CheckoutItems />
+            </div>
+
+            {/* Right */}
+            <OrderSummary actionText="Confirm Order" actionLink="#" />
           </div>
-
-          {/* Right */}
-          <OrderSummary actionText="Confirm Order" actionLink="#" />
         </div>
-      </div>
+      </CheckoutContextProvider>
     </CartContextProvider>
   );
 };
--- src/components/checkout/CheckoutForm.jsx
+++ src/components/checkout/CheckoutForm.jsx
@@ -1,16 +1,42 @@
+"use client";
+
+import useCheckout from "@/hooks/useCheckout";
 
 const CheckoutForm = () => {
+  const { customerInfo, handleValueChange } = useCheckout();
+
   return (
     <div className="card bg-base-100 border border-base-200">
       <div className="card-body">
         <h2 className="font-semibold text-lg">ডেলিভারি তথ্য</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-          <input className="input input-bordered" placeholder="নাম" />
-          <input className="input input-bordered" placeholder="মোবাইল নম্বর" />
           <input
-            className="input input-bordered md:col-span-2"
+            type="text"
+            className="input input-bordered w-full"
+            placeholder="নাম"
+            value={customerInfo?.name || ""}
+            onChange={(e) => handleValueChange("name", e.target.value)}
+          />
+          <input
+            type="email"
+            className="input input-bordered w-full"
+            placeholder="ইমেইল"
+            value={customerInfo?.email || ""}
+            onChange={(e) => handleValueChange("email", e.target.value)}
+          />
+          <input
+            type="text"
+            className="input input-bordered w-full md:col-span-2"
+            placeholder="মোবাইল নম্বর"
+            value={customerInfo?.phone || ""}
+            onChange={(e) => handleValueChange("phone", e.target.value)}
+          />
+          <textarea
+            className="textarea w-full md:col-span-2"
             placeholder="সম্পূর্ণ ঠিকানা"
+            value={customerInfo?.address || ""}
+            onChange={(e) => handleValueChange("address", e.target.value)}
           />
         </div>
       </div>
